ce995fa90fda0cfb826973ac18f38338b61c0435
Vivian28 <vivian.michel@orange.fr>
2022-02-20 13:46:29 +0100

    ajout carousel réalisation

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const body = document.body,
-    scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
-    height = scrollWrap.getBoundingClientRect().height-1,
-    speed = 0.04;
+  scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+  height = scrollWrap.getBoundingClientRect().height - 1,
+  speed = 0.04;
 
 var offset = 0;
 
+const imageveto = document.querySelector(".image-veto");
+const carrousel = document.querySelector(".carrousel");
+
+const carrouselSlide = document.querySelector(".carrousel-slide");
+const carrouselImage = document.querySelectorAll(".carrousel-slide img");
+
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+let counter = 1;
+const size = carrouselImage[0].clientWidth;
+
 body.style.height = Math.floor(height) + "px";
 
-function smoothScroll(){
-    offset += (window.pageYOffset - offset) * speed;
-    
-    var scroll = "translateY(-" + offset + "px) translateZ(0)";
-    scrollWrap.style.transform = scroll;
+function smoothScroll() {
+  offset += (window.pageYOffset - offset) * speed;
 
-    callScroll = requestAnimationFrame(smoothScroll);
+  var scroll = "translateY(-" + offset + "px) translateZ(0)";
+  scrollWrap.style.transform = scroll;
+
+  callScroll = requestAnimationFrame(smoothScroll);
 }
 
 //smoothScroll();
