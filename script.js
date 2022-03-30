83afec3465b897b33cc1834592b28e3759b984e4
Vivian28 <vivian.michel@orange.fr>
2022-03-30 17:52:13 +0200

    changement footer

--- script.js
+++ script.js
@@ -5,15 +5,4 @@ const body = document.body,
 
 var offset = 0;
 
-const imageveto = document.querySelector(".image-veto");
-const carrousel = document.querySelector(".carrousel");
-
-const carrouselSlide = document.querySelector(".carrousel-slide");
-const carrouselImage = document.querySelectorAll(".carrousel-slide img");
-
-const prevBtn = document.querySelector("#prevBtn");
-const nextBtn = document.querySelector("#nextBtn");
-
-
-
 //smoothScroll();
